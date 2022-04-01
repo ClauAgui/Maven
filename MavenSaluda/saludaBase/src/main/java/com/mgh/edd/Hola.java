ed0cf452b54cf32eb3ccf8ad778337ca803008e6
claudia <clo.aguinaco.s@gmail.com>
2022-04-01 20:40:33 +0200

    Saluda Maven desde Eclipse

---
+++ MavenSaluda/saludaBase/src/main/java/com/mgh/edd/Hola.java
@@ -0,0 +1,12 @@
+package com.mgh.edd;
+
+public class Hola {
+
+
+    public static void main(String[] args) {
+        String nombre=args.length>0?args[0]:"";
+        Saluda.saluda(nombre);
+    }
+}
+
+  
